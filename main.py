f4a97e45fd2662ad613e3757a5e71768a097ac72
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2024-01-01 13:57:25 +0500

    Scrollbar for response widget added

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ import os
 import pickle
 
 # Create the main window of the app.
-root = customtkinter.CTk()
-root.title("PyChat")
-root.geometry("800x600")
+app = customtkinter.CTk()
+app.title("PyChat")
+app.geometry("800x600")
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
+# Create frame for the textbox
+textbox_frame = customtkinter.CTkFrame(app)
+textbox_frame.pack(pady=20)
+
+# Create the textbox for the responses
+textbox = Text(textbox_frame, bg="#343638", width=65, bd=1, fg="#d6d6d6", relief="flat", wrap=WORD,
+               selectbackground="#1f538d")
+textbox.grid(row=0, column=0)
+
+# Add a scrollbar to the response widget
+response_scrollbar = customtkinter.CTkScrollbar(textbox_frame, command=textbox.yview)
+response_scrollbar.grid(row=0, column=1, sticky="ns")
+
+# Add the scrollbar inside the response widget
+textbox.configure(yscrollcommand=response_scrollbar.set)
+
 # run the main app
-root.mainloop()
+app.mainloop()
