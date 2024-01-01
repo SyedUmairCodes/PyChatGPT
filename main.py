f3c2427f9252064d1c6a22829346f19c12091733
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2024-01-01 13:21:53 +0500

    Initialized app

---
+++ main.py
@@ -0,0 +1,16 @@
+# Import required libraries.
+from tkinter import *
+import customtkinter
+import openai
+import os
+import pickle
+
+# Create the main window of the app.
+root = customtkinter.CTk()
+root.title("PyChat")
+root.geometry("800x600")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+# run the main app
+root.mainloop()
